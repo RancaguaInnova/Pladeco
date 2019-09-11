34c9082fb8688fa32a904b2249814c6d4acb8f0e
JEPEREZR <48518017+JEPEREZR@users.noreply.github.com>
2019-09-11 17:51:30 -0300

    correcciones

--- src/components/users/create.js
+++ src/components/users/create.js
@@ -1,23 +1,48 @@
 import React from 'react'
 import { TextInput, DateInput, SimpleForm, SelectInput, DisabledInput, Create } from 'react-admin'
 import InputSearchPlace from '../../helpers/fields/inputSearchPlace'
-const UserEdit = props => (
+import _setWith from 'lodash/setWith'
+import  EmailValidator  from 'email-validator'
+const validateUserCreation = (values) => {
+  const errors = {};
+  if (!values.profile || !values.profile.firstName ) {
+    _setWith(errors,'profile.firstName','Debe agregar el nombre del usuario',errors)
+  }
+  if (!values.profile || !values.profile.lastName) {
+    _setWith(errors,'profile.lastName','Debe agregar el apellido del usuario',errors)
+  } 
+  if (!values.email) {
+    errors.email = ['Debe agregar un email'];
+  }  
+  if (values.email!=="" && EmailValidator.validate(values.email)===false) {
+    errors.email = ['Debe agregar un email válido'];
+  } 
+  if (!values.password) {
+    errors.password = ['La contraseña es requerida'];
+  } 
+  if (!values.profile || !values.profile.identifier) {
+    _setWith(errors,'profile.identifier','Debe agregar el número de documento del usuario (R.U.T)',errors)
+  }   
+  return errors
+};
+const UserEdit = props => {
+  console.log(props)
+  return(
   <Create title='Crear Usuario' {...props}>
-    <SimpleForm>
-      <DisabledInput source='id' />
-      <TextInput source='profile.firstName' label='Nombre' />
-      <TextInput source='profile.lastName' label='Apellido' />
-      <TextInput source='email' label='Email' />
-      <TextInput source='password' label='Contraseña' />
-      <TextInput source='profile.identifier' label='Número de Documento' />
+    <SimpleForm validate={validateUserCreation}>
+      <TextInput source='profile.firstName' label='Nombre'  defaultValue='' />
+      <TextInput source='profile.lastName' label='Apellido'  defaultValue='' />
+      <TextInput source='email' label='Email' type="email"  defaultValue='' />
+      <TextInput source='password' label='Contraseña'  defaultValue='' />
+      <TextInput source='profile.identifier' label='Número de Documento'   defaultValue=''/>
       <SelectInput
         source='profile.gender'
         choices={[{ id: 'hombre', name: 'hombre' }, { id: 'mujer', name: 'mujer' }]}
         label='Sexo'
       />
-      <DateInput source='profile.birthdate' label='Fecha de Nacimiento' />
+      <DateInput source='profile.birthdate' label='Fecha de Nacimiento'/>
       <InputSearchPlace source='profile.address'></InputSearchPlace>
     </SimpleForm>
   </Create>
-)
+)}
 export default UserEdit
--- src/components/users/edit.js
+++ src/components/users/edit.js
@@ -13,19 +13,15 @@ import { DateInput } from 'react-admin-date-inputs'
 const UserEdit = props => (
   <Edit {...props} title="Editando usuario">
     <SimpleForm>
-      <DisabledInput source='id' />
       <TextInput source='profile.firstName' label='Nombre' />
       <TextInput source='profile.lastName' label='Apellido' />
       <SelectInput
         source='profile.gender'
         choices={[{ id: 'hombre', name: 'hombre' }, { id: 'mujer', name: 'mujer' }]}
         label='Sexo'
-      />
-      <DateInput
-        source='profile.birthdate'
-        label='Fecha de Nacimiento'
-        options={{ format: 'dd/MM/YYYY' }}
-      />
+      />      <DateInput source='profile.birthdate' label='Fecha de Nacimiento'/>
+
+   
     </SimpleForm>
   </Edit>
 )
