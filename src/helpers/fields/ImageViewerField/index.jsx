07dfc16ca8a556b265273b83b46427ab58f28299
JEPEREZR <48518017+JEPEREZR@users.noreply.github.com>
2019-11-15 19:20:32 -0300

    search  fix

--- src/helpers/fields/ImageViewerField/index.jsx
+++ src/helpers/fields/ImageViewerField/index.jsx
@@ -4,17 +4,22 @@ import Viewer from 'react-viewer';
 import './styles.scss'  
 const ImageViewerField = ({ source, record,label = {} }) => {
     const [visible, setVisible] = React.useState(false);
-
+console.log(record[source])
     return (
         <div>
             <div>{label}</div>
+
+           {/*  {record[source].map((item,i) => {
             <div onClick={() => { setVisible(true); }}> <img className='ImageViewerField' src={record[source].src} title={record[source].title}></img>
             </div>
             <Viewer
                 visible={visible}
                 onClose={() => { setVisible(false); }}
                 images={[{ src: record[source].src, alt: record[source].title }]}
-            />
+            />}
+            )} */}
+            
+
         </div>
     )
 }
