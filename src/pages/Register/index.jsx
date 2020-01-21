5902eab5ff8553d56059361d854dd9fabd088bed
Joaquin Gumucio L <jjgumucio@gmail.com>
2020-01-21 16:23:38 -0300

    Fix regex

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import Alert from "../Alert";
-import { makeStyles } from "@material-ui/core/styles";
-import * as firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+import React, { useState } from "react"
+import Button from "@material-ui/core/Button"
+import TextField from "@material-ui/core/TextField"
+import Alert from "../Alert"
+import { makeStyles } from "@material-ui/core/styles"
+import * as firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -14,11 +14,9 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
-};
+}
 
-const Firebase = firebase.initializeApp(firebaseConfig, "AUTH");
-
-
+const Firebase = firebase.initializeApp(firebaseConfig, "AUTH")
 
 export default function Registro() {
   const useStyles = makeStyles(theme => ({
@@ -49,30 +47,30 @@ export default function Registro() {
     submit: {
       margin: theme.spacing(3, 0, 2)
     }
-  }));
+  }))
 
-  const classes = useStyles();
+  const classes = useStyles()
 
-  let [email, setEmail] = useState("");
-  let [password, setPassword] = useState("");
-  let [repeatedPassword, setRepeteadPassword] = useState("");
+  let [email, setEmail] = useState("")
+  let [password, setPassword] = useState("")
+  let [repeatedPassword, setRepeteadPassword] = useState("")
 
   const handleEmailChange = event => {
-    setEmail(event.target.value);
-  };
+    setEmail(event.target.value)
+  }
 
   const handlePasswordChange = event => {
-    setPassword(event.target.value);
-  };
+    setPassword(event.target.value)
+  }
 
   const handleRepeatPasswordChange = event => {
-    setRepeteadPassword(event.target.value);
-  };
+    setRepeteadPassword(event.target.value)
+  }
 
-  const [alert, setAlert] = useState({ open: false, title: "", body: "" });
+  const [alert, setAlert] = useState({ open: false, title: "", body: "" })
 
   const submit = async e => {
-    e.preventDefault();
+    e.preventDefault()
 
     // Validation
     if (password !== repeatedPassword) {
@@ -80,57 +78,57 @@ export default function Registro() {
         open: true,
         title: "Contraseñas no coinciden!",
         body: "Por favor verifique que las constraseñas coinsidan"
-      });
-      return;
+      })
+      return
     }
     if (password.length < 6) {
       setAlert({
         open: true,
         title: "Contraseña inválida!",
         body: "Por favor ingrese una constraseña de al menos 6 caracteres"
-      });
-      return;
+      })
+      return
     }
     if (!email || !password || !repeatedPassword) {
       setAlert({
         open: true,
         title: "Formulario inclompleto!",
         body: "Por favor rellene todos los campos"
-      });
-      return;
+      })
+      return
     }
-    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+    if (!/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
       setAlert({
         open: true,
         title: "Email inválido!",
         body: "Por favor verifique su email"
-      });
-      return;
+      })
+      return
     }
 
     try {
       const {
         user: { uid }
-      } = await Firebase.auth().createUserWithEmailAndPassword(email, password);
+      } = await Firebase.auth().createUserWithEmailAndPassword(email, password)
       Firebase.firestore()
         .collection("users")
         .doc(uid)
-        .set({ email: { address: email } });
+        .set({ email: { address: email } })
 
       setAlert({
         open: true,
         title: "Usuario creado!",
         body: `Su usuario ha sido creado exitosamente. Contacte a su administrador para que le asigne sus permisos.`
-      });
+      })
     } catch (error) {
       setAlert({
         open: true,
         title: "Error!",
         body: `Ocurrión un error al registrar el usuario: ${error.message}`
-      });
-      return;
+      })
+      return
     }
-  };
+  }
 
   return (
     <form className={classes.form} noValidate onSubmit={submit}>
@@ -183,5 +181,5 @@ export default function Registro() {
 
       <Alert handleClose={() => setAlert({ open: false })} {...alert} />
     </form>
-  );
+  )
 }
