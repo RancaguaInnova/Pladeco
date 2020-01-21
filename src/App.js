d03ac13c8531008e9fa669cba296c712bef72e95
Joaquin Gumucio L <jjgumucio@gmail.com>
2020-01-21 16:21:13 -0300

    Deletes unused imports

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import React from "react";
-import { Admin, Resource } from "react-admin";
-import Users from "./pages/users";
-import Action from "./pages/action";
-import Activity from "./pages/activity";
-import Deparment from "./pages/department";
-import Objective from "./pages/objective";
-import Line from "./pages/line";
-import WorkPlan from "./pages/workPlan";
-import Dashboard from "./pages/dashboard";
-import Roles from "./pages/roles";
-import Area from "./pages/area";
-import { Layout } from "./pages/layout";
-import CustomRouters from "./pages/customRoutes";
-import spanishMessages from "@blackbox-vision/ra-language-spanish";
-import polyglotI18nProvider from "ra-i18n-polyglot";
-import AddUploadCapabilities from "./helpers/UploadImage";
-import { ResourceWithPermissions } from "ra-auth-acl";
-import Profile from "./pages/profile";
-import AuthProvider from "./auth";
-import { FirebaseDataProvider } from "react-admin-firebase";
-import Login from "./pages/login";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Register from "./pages/Register";
-import Inicio from "./pages/Inicio";
-const i18nProvider = polyglotI18nProvider(() => spanishMessages, "es");
+import React from "react"
+import { Admin } from "react-admin"
+import Users from "./pages/users"
+import Action from "./pages/action"
+import Activity from "./pages/activity"
+import Deparment from "./pages/department"
+import Objective from "./pages/objective"
+import Line from "./pages/line"
+import WorkPlan from "./pages/workPlan"
+import Roles from "./pages/roles"
+import Area from "./pages/area"
+import { Layout } from "./pages/layout"
+import CustomRouters from "./pages/customRoutes"
+import spanishMessages from "@blackbox-vision/ra-language-spanish"
+import polyglotI18nProvider from "ra-i18n-polyglot"
+import AddUploadCapabilities from "./helpers/UploadImage"
+import { ResourceWithPermissions } from "ra-auth-acl"
+import Profile from "./pages/profile"
+import AuthProvider from "./auth"
+import { FirebaseDataProvider } from "react-admin-firebase"
+import Inicio from "./pages/Inicio"
+const i18nProvider = polyglotI18nProvider(() => spanishMessages, "es")
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -32,14 +28,14 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
-};
+}
 
 const options = {
   logging: false
-};
+}
 
-const dataProvider = FirebaseDataProvider(firebaseConfig, options);
-const authProvider = AuthProvider();
+const dataProvider = FirebaseDataProvider(firebaseConfig, options)
+const authProvider = AuthProvider()
 
 const App = () => (
   <div>
@@ -53,59 +49,19 @@ const App = () => (
     >
       {permissions => {
         return [
-          <ResourceWithPermissions
-            name="workplans"
-            {...WorkPlan}
-            permissions={permissions}
-          />,
-          <ResourceWithPermissions
-            name="areas"
-            {...Area}
-            permissions={permissions}
-          />,
-          <ResourceWithPermissions
-            name="lines"
-            {...Line}
-            permissions={permissions}
-          />,
-          <ResourceWithPermissions
-            name="objectives"
-            {...Objective}
-            permissions={permissions}
-          />,
-          <ResourceWithPermissions
-            name="actions"
-            {...Action}
-            permissions={permissions}
-          />,
-          <ResourceWithPermissions
-            name="activities"
-            {...Activity}
-            permissions={permissions}
-          />,
-          <ResourceWithPermissions
-            name="departments"
-            {...Deparment}
-            permissions={permissions}
-          />,
-          <ResourceWithPermissions
-            name="roles"
-            {...Roles}
-            permissions={permissions}
-          />,
-          <ResourceWithPermissions
-            name="users"
-            {...Users}
-            permissions={permissions}
-          />,
-          <ResourceWithPermissions
-            name="profile"
-            {...Profile}
-            permissions={permissions}
-          />
-        ];
+          <ResourceWithPermissions name="workplans" {...WorkPlan} permissions={permissions} />,
+          <ResourceWithPermissions name="areas" {...Area} permissions={permissions} />,
+          <ResourceWithPermissions name="lines" {...Line} permissions={permissions} />,
+          <ResourceWithPermissions name="objectives" {...Objective} permissions={permissions} />,
+          <ResourceWithPermissions name="actions" {...Action} permissions={permissions} />,
+          <ResourceWithPermissions name="activities" {...Activity} permissions={permissions} />,
+          <ResourceWithPermissions name="departments" {...Deparment} permissions={permissions} />,
+          <ResourceWithPermissions name="roles" {...Roles} permissions={permissions} />,
+          <ResourceWithPermissions name="users" {...Users} permissions={permissions} />,
+          <ResourceWithPermissions name="profile" {...Profile} permissions={permissions} />
+        ]
       }}
     </Admin>
   </div>
-);
-export default App;
+)
+export default App
