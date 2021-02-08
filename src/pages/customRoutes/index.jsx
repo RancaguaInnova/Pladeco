2817d3c02e2a6c4c004cba5ad05dc38ddd09251d
Jaime Perez <jaime.e.perez.r@hotmail.com>
2021-02-08 17:33:03 -0300

    fix

--- src/pages/customRoutes/index.jsx
+++ src/pages/customRoutes/index.jsx
@@ -3,9 +3,12 @@ import { Route } from "react-router-dom";
 import { AccionValidation, AccionEdit } from "../activityValidation";
 import Dashboard from "../dashboard";
 import Register from "../Register";
+import ActionAll from "../action/list_all"
 
 export default [
   <Route exact path="/" component={Dashboard} />,
+  <Route exact path="/accion/all" component={ActionAll} />,
+
   <Route exact path="/AccionValidation" component={AccionValidation} />,
   <Route exact path="/AccionValidation/:id" component={AccionEdit} />,
   <Route path="/registro" render={() => <Register />} noLayout />
